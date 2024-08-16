7b1eee8727e393dff8a964b6ef21302e8d95bc8e
Ighodalo Ellis <32099998+IghodaloEllis@users.noreply.github.com>
2024-08-16 15:28:17 +0100

    Delete sql/student_Achievements.sql

--- sql/student_Achievements.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Student_Achievements (
-    achievement_id INT PRIMARY KEY AUTO_INCREMENT,
-    student_id INT,
-    achievement_name VARCHAR(255),
-    description TEXT,
-    date_achieved DATE,
-    proof_link VARCHAR(255), -- Link to proof of achievement
-    FOREIGN KEY (student_id) REFERENCES Students(student_id)
-);
