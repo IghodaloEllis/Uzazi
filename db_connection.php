3f31c0f9b16b4e6aa1b946cd153b215f2bbc8674
Ighodalo Ellis <32099998+IghodaloEllis@users.noreply.github.com>
2024-08-16 11:47:46 +0100

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,19 @@
+<?php
+// db_connection.php
+
+function getConnection() {
+    $servername = "your_servername";
+    $username = "your_username";
+    $password = "your_password";
+    $dbname = "your_database";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
